2d577e004b7e42683752c7a5f113209131629e26
Juan Diaz <juannavegante2010@gmail.com>
2023-12-30 00:15:43 -0500

    cambios

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 // const http = require("http");
 // const server = http.createServer();
-// const { Socket } = require("socket.io");
+// const { Server: IoServer } = require("socket.io");  // Renombré Socket a IoServer para claridad
 
-// const PORT = 4000
-// const io = require('socket.io')(server,{
-//     cors:{ origin: '*'}
-// })
-// io.on('connection', (socket) =>{
-//     console.log("Conexión establecida")
-//     socket.on('change_state',(data)=>{
-//         io.emit('change_state',data);
-//     })
-// })
+// const PORT = 4000;
 
-// server.listen(PORT);
+// // Asegúrate de escuchar en IPv4 (0.0.0.0) para permitir conexiones desde cualquier dirección IPv4.
+// const io = new IoServer(server, {
+//     cors: { origin: '*' },
+//     transports: ['polling', 'websocket']
+// });
 
-const http = require("http");
-const server = http.createServer();
-const { Server: IoServer } = require("socket.io");  // Renombré Socket a IoServer para claridad
+// io.on('connection', (socket) => {
+//     console.log("Conexión establecida");
 
-const PORT = 4000;
+//     socket.on('change_state', (data) => {
+//         io.emit('change_state', data);
+//     });
+// });
 
-// Asegúrate de escuchar en IPv4 (0.0.0.0) para permitir conexiones desde cualquier dirección IPv4.
-const io = new IoServer(server, {
-    cors: { origin: '*' },
-    transports: ['polling', 'websocket']
+// // Escucha en todas las direcciones IPv4 en el puerto especificado.
+// server.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Servidor escuchando en el puerto ${PORT}`);
+// });
+
+
+const http = require('http');
+
+// Crea el servidor
+const server = http.createServer((req, res) => {
+    // Establece la cabecera de la respuesta con un código 200 (OK)
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    // Envía la respuesta al cliente
+    res.end('¡Hola desde el servidor de Node.js!\n');
 });
 
-io.on('connection', (socket) => {
-    console.log("Conexión establecida");
-
-    socket.on('change_state', (data) => {
-        io.emit('change_state', data);
-    });
-});
-
-// Escucha en todas las direcciones IPv4 en el puerto especificado.
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+// Escucha en el puerto 3000
+server.listen(3000, '0.0.0.0', () => {
+    console.log('El servidor está escuchando en http://127.0.0.1:3000/');
 });
